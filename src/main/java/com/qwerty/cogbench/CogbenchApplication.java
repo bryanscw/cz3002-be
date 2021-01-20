4d00e7c7e39dfa17450b836c3ae57637eb123d4a
voonhous <voonhousu@gmail.com>
2021-01-20 18:39:25 +0800

    Initialise Spring project (#1)

---
+++ src/main/java/com/qwerty/cogbench/CogbenchApplication.java
@@ -0,0 +1,13 @@
+package com.qwerty.cogbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogbenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogbenchApplication.class, args);
+	}
+
+}
