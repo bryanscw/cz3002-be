34d556c5eea3bce88f7c08457f096fd5aca98389
suvoonhou <voonhousu@gmail.com>
2021-01-29 15:38:14 +0800

    Adding test properties

--- src/test/java/com/qwerty/cogbench/CogbenchApplicationTests.java
+++ src/test/java/com/qwerty/cogbench/CogbenchApplicationTests.java
@@ -2,8 +2,10 @@ package com.qwerty.cogbench;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CogbenchApplicationTests {
 
 	@Test
