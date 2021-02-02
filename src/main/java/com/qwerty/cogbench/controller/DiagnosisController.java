f195a52905a4998782400ada8b89a5ffead315fe
suvoonhou <voonhousu@gmail.com>
2021-02-02 14:38:12 +0800

    Adding placeholder files

---
+++ src/main/java/com/qwerty/cogbench/controller/DiagnosisController.java
@@ -0,0 +1,8 @@
+package com.qwerty.cogbench.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DiagnosisController {
+
+}
---
+++ src/main/java/com/qwerty/cogbench/model/Result.java
@@ -0,0 +1,16 @@
+package com.qwerty.cogbench.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "result")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Result {
+
+}
