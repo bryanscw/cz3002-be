1f41e26b81a0665c352bfd70f7e2a3fac3dde3cc
suvoonhou <voonhousu@gmail.com>
2021-02-02 16:15:19 +0800

    Adding id field

--- src/main/java/com/qwerty/cogbench/model/Result.java
+++ src/main/java/com/qwerty/cogbench/model/Result.java
@@ -1,9 +1,14 @@
 package com.qwerty.cogbench.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
@@ -12,5 +17,11 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class Result {
+
+  @Id
+  @Getter
+  @Setter
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
 }
