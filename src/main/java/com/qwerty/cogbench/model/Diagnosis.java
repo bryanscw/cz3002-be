78dd970378980786ab5aadff1dae060c7fad5354
suvoonhou <voonhousu@gmail.com>
2021-02-03 16:47:35 +0800

    Added fields for Diagnosis and Result model

--- src/main/java/com/qwerty/cogbench/model/Diagnosis.java
+++ src/main/java/com/qwerty/cogbench/model/Diagnosis.java
@@ -1,27 +1,47 @@
 package com.qwerty.cogbench.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "diagnosis")
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-public class Diagnosis {
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Diagnosis extends Auditable<String> {
 
   @Id
   @Getter
   @Setter
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Getter
+  @Setter
+  @OneToOne
+  private Result result;
+
+  @Getter
+  @Setter
+  @OneToOne
+  private User professional;
+
+  @Getter
+  @Setter
+  private String label;
+
+  @Getter
+  @Setter
+  private String description;
 
 }
