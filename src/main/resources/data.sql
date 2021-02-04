4ee59b835867713bf745e0e42d074c41465ac06d
bryanscw <csin001@e.ntu.edu.sg>
2021-02-04 16:59:21 +0800

    Added comments for account credentials for accounts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,17 @@
+-- Username: admin1@test.com, Password: admin01
 INSERT INTO qwerty.app_user (email, pass, role, name, dob, gender) VALUES ('admin1@test.com', '$2y$12$xT.zKSUW69yMuKT5oV.BL.dumTRndnEksLbkRQ2Gn.6/j2YvOShM.', 'ROLE_ADMIN', 'admin1', '2005-05-02', '1');
+
+-- Username: admin2@test.com, Password: admin02
 INSERT INTO qwerty.app_user (email, pass, role, name, dob, gender) VALUES ('admin2@test.com', '$2y$12$fQ/wnljrr9c/.N4EEr5eeOf0yFEn1OxawCZHI0vxhnDZ.2keDwEpy', 'ROLE_ADMIN', 'admin2', '2009-06-27', '0');
+
+-- Username: candidate1@test.com, Password: candidate01
 INSERT INTO qwerty.app_user (email, pass, role, name, dob, gender) VALUES ('candidate1@test.com', '$2y$12$FjlQ8Tbs9T6Z4mNEDB/T9uMlar3gu9NcPwmMMAQgnl5bVaqTnlGZ6', 'ROLE_CANDIDATE', 'candidate1', '1967-01-02', '1');
+
+-- Username: candidate2@test.com, Password: candidate02
 INSERT INTO qwerty.app_user (email, pass, role, name, dob, gender) VALUES ('candidate2@test.com', '$2y$12$ohMJVj0MXBeoG5u5YX69kuLRjewbv1h3xiK1I7FrBm7wi2XGdeNPq', 'ROLE_CANDIDATE', 'candidate2', '1956-09-11', '0');
+
+-- Username: doctor1@test.com, Password: doctor01
 INSERT INTO qwerty.app_user (email, pass, role, name, dob, gender) VALUES ('doctor1@test.com', '$2y$12$bz0DAPR/tl9g1RCM7Xcd1uRk22be4UUtcisBBmCuRSkbTBteP4/pW ', 'ROLE_DOCTOR', 'doctor1', '1997-01-02', '1');
+
+-- Username: doctor2@test.com, Password: doctor02
 INSERT INTO qwerty.app_user (email, pass, role, name, dob, gender) VALUES ('doctor2@test.com', '$2y$12$oK0EgHJ5Ya.pIoAwy77kaeTFfec6OW0N/NyVPPQY40AfLvwNF36Oi', 'ROLE_DOCTOR', 'doctor2', '1984-09-11', '0');
