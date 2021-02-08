e9f93327ca38cb7d82c88fcd238c380a969a30be
bryanscw <csin001@e.ntu.edu.sg>
2021-02-08 17:57:36 +0800

    Added result controller + test

--- src/main/java/com/qwerty/cogbench/repository/ResultRepository.java
+++ src/main/java/com/qwerty/cogbench/repository/ResultRepository.java
@@ -1,8 +1,20 @@
 package com.qwerty.cogbench.repository;
 
+import com.qwerty.cogbench.model.Result;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ResultRepository {
+public interface ResultRepository extends PagingAndSortingRepository<Result, Integer> {
+
+    Optional<Result> findResultByUserEmail(String email);
+
+    Optional<Result> findResultById(Integer id);
+
+    Optional<Page<Result>> findAllResultByUserEmail(String email, Pageable pageable);
 
 }
