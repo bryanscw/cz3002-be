15f740fea3e35bdaa1bb65e72ff44eb839c2056d
suvoonhou <voonhousu@gmail.com>
2021-02-09 12:53:08 +0800

    Fix Spring-REST-docs not being packaged and generated in deployment

--- src/main/java/com/qwerty/cogbench/model/Diagnosis.java
+++ src/main/java/com/qwerty/cogbench/model/Diagnosis.java
@@ -3,9 +3,15 @@ package com.qwerty.cogbench.model;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
-import javax.persistence.*;
-
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
