3876b2ab891083345140e3b05c50844447608863
voonhous <voonhousu@gmail.com>
2021-02-15 18:30:26 +0800

    Perform code cleanup (#28)
    * Changed Diagnosis to User relationship
    
    * Fix Result and Diagnosis controller not returning correct values
    
    * Perform code cleanup

--- src/main/java/com/qwerty/cogbench/service/ResultService.java
+++ src/main/java/com/qwerty/cogbench/service/ResultService.java
@@ -7,14 +7,14 @@ import org.springframework.security.core.Authentication;
 
 public interface ResultService {
 
-    Result create(String userEmail, Result result, Authentication authentication);
+  Result create(String userEmail, Result result, Authentication authentication);
 
-    Result getLatestResult(String email, Authentication authentication);
+  Result getLatestResult(String email, Authentication authentication);
 
-    boolean delete(String userEmail, Integer resultId, Authentication authentication);
+  boolean delete(String userEmail, Integer resultId, Authentication authentication);
 
-    Page<Result> getHistory(String email, Pageable pageable, Authentication authentication);
+  Page<Result> getHistory(String email, Pageable pageable, Authentication authentication);
 
-    Page<Result> fetchAll(Pageable pageable);
+  Page<Result> fetchAll(Pageable pageable);
 
 }
--- src/main/java/com/qwerty/cogbench/service/ResultServiceImpl.java
+++ src/main/java/com/qwerty/cogbench/service/ResultServiceImpl.java
@@ -16,93 +16,94 @@ import org.springframework.stereotype.Service;
 @Service
 public class ResultServiceImpl implements ResultService {
 
-    private final ResultRepository resultRepository;
+  private final ResultRepository resultRepository;
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    public ResultServiceImpl(ResultRepository resultRepository, UserRepository userRepository){
-        this.resultRepository = resultRepository;
-        this.userRepository = userRepository;
-    }
+  public ResultServiceImpl(ResultRepository resultRepository, UserRepository userRepository) {
+    this.resultRepository = resultRepository;
+    this.userRepository = userRepository;
+  }
 
-    @Override
-    public Page<Result> fetchAll(Pageable pageable) {
-        return resultRepository.findAll(pageable);
-    }
+  @Override
+  public Page<Result> fetchAll(Pageable pageable) {
+    return resultRepository.findAll(pageable);
+  }
 
-    @Override
-    public Result create(String userEmail, Result result, Authentication authentication) {
-        // Check if user is authorized to perform action
-        this.isAuthorized(userEmail, authentication);
+  @Override
+  public Result create(String userEmail, Result result, Authentication authentication) {
+    // Check if user is authorized to perform action
+    this.isAuthorized(userEmail, authentication);
 
-        User userToFind = userRepository.findUserByEmail(userEmail).orElseThrow(() -> {
-            String errorMsg = String.format("User with email [%s] not found", userEmail);
-            log.error(errorMsg);
-            return new ResourceNotFoundException(errorMsg);
+    User userToFind = userRepository.findUserByEmail(userEmail).orElseThrow(() -> {
+      String errorMsg = String.format("User with email [%s] not found", userEmail);
+      log.error(errorMsg);
+      return new ResourceNotFoundException(errorMsg);
+    });
+
+    result.setUser(userToFind);
+
+    return resultRepository.save(result);
+  }
+
+  @Override
+  public Result getLatestResult(String userEmail, Authentication authentication) {
+    // Check if user is authorized to perform action
+    this.isAuthorized(userEmail, authentication);
+
+    return resultRepository.findResultByUserEmail(userEmail).orElseThrow(
+        () -> new ResourceNotFoundException(
+            String.format("User with email [%s] not found", userEmail)));
+  }
+
+  public Page<Result> getHistory(String userEmail, Pageable pageable,
+      Authentication authentication) {
+    // Check if user is authorized to perform action
+    this.isAuthorized(userEmail, authentication);
+
+    return resultRepository.findAllResultByUserEmail(userEmail, pageable).orElseThrow(
+        () -> new ResourceNotFoundException(
+            String.format("Results for user [%s] not found", userEmail)));
+  }
+
+  @Override
+  public boolean delete(String userEmail, Integer resultId, Authentication authentication) {
+
+    Result resultToFind = resultRepository
+        .findResultById(resultId)
+        .orElseThrow(() -> {
+          String progressErrorMsg = String
+              .format("Result for User with userEmail: [%s] and resultId: [%s] not found",
+                  userEmail, resultId);
+          log.error(progressErrorMsg);
+          throw new ResourceNotFoundException(progressErrorMsg);
         });
 
-        result.setUser(userToFind);
+    // Delete the Progress
+    resultRepository.deleteById(resultToFind.getId());
 
-        return resultRepository.save(result);
+    return true;
+  }
+
+  /**
+   * Check if userEmail is the same as that of that defined in the authentication context.
+   * <p>
+   * This check is for actions where users are only allowed to modify their own resources.
+   *
+   * @param userEmail      User email.
+   * @param authentication Authentication context containing information of the user submitting the
+   *                       request.
+   */
+  private void isAuthorized(String userEmail, Authentication authentication) {
+    String principalName = ((org.springframework.security.core.userdetails.User) authentication
+        .getPrincipal()).getUsername();
+
+    if (!userEmail.equals(principalName)) {
+      String errorMsg = String.format(
+          "User with userEmail: [%s] is not allowed to modify resources for user with userEmail: [%s]",
+          principalName, userEmail);
+      throw new UnauthorizedException(errorMsg);
     }
 
-    @Override
-    public Result getLatestResult(String userEmail, Authentication authentication) {
-        // Check if user is authorized to perform action
-        this.isAuthorized(userEmail, authentication);
-
-        return resultRepository.findResultByUserEmail(userEmail).orElseThrow(
-                () -> new ResourceNotFoundException(
-                        String.format("User with email [%s] not found", userEmail)));
-    }
-
-    public Page<Result> getHistory(String userEmail, Pageable pageable, Authentication authentication) {
-        // Check if user is authorized to perform action
-        this.isAuthorized(userEmail, authentication);
-
-        return resultRepository.findAllResultByUserEmail(userEmail, pageable).orElseThrow(
-                () -> new ResourceNotFoundException(
-                        String.format("Results for user [%s] not found", userEmail)));
-    }
-
-    @Override
-    public boolean delete(String userEmail, Integer resultId, Authentication authentication) {
-
-        Result resultToFind = resultRepository
-                .findResultById(resultId)
-                .orElseThrow(() -> {
-                    String progressErrorMsg = String
-                            .format("Result for User with userEmail: [%s] and resultId: [%s] not found",
-                                    userEmail, resultId);
-                    log.error(progressErrorMsg);
-                    throw new ResourceNotFoundException(progressErrorMsg);
-                });
-
-        // Delete the Progress
-        resultRepository.deleteById(resultToFind.getId());
-
-        return true;
-    }
-
-    /**
-     * Check if userEmail is the same as that of that defined in the authentication context.
-     * <p>
-     * This check is for actions where users are only allowed to modify their own resources.
-     *
-     * @param userEmail      User email.
-     * @param authentication Authentication context containing information of the user submitting the
-     *                       request.
-     */
-    private void isAuthorized(String userEmail, Authentication authentication) {
-        String principalName = ((org.springframework.security.core.userdetails.User) authentication
-                .getPrincipal()).getUsername();
-
-        if (!userEmail.equals(principalName)) {
-            String errorMsg = String.format(
-                    "User with userEmail: [%s] is not allowed to modify resources for user with userEmail: [%s]",
-                    principalName, userEmail);
-            throw new UnauthorizedException(errorMsg);
-        }
-
-    }
+  }
 }
