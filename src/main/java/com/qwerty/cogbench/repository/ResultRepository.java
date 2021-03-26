2c84e069756d2faa82c1e93b54ccb669f4b2cd56
voonhous <voonhousu@gmail.com>
2021-03-26 21:32:58 +0800

    Fix graph and revert to POST for fetch results (#59)
    * Fix graph and revert to POST for fettch results
    
    * Fix tests

--- src/main/java/com/qwerty/cogbench/repository/ResultRepository.java
+++ src/main/java/com/qwerty/cogbench/repository/ResultRepository.java
@@ -2,26 +2,24 @@ package com.qwerty.cogbench.repository;
 
 import com.qwerty.cogbench.model.Result;
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Integer> {
 
-    Optional<Result> findFirstByUserEmailOrderByLastModifiedDateDesc(String email);
+  Optional<Result> findFirstByUserEmailOrderByLastModifiedDateDesc(String email);
 
-    Optional<Result> findResultById(Integer id);
+  Optional<Result> findResultById(Integer id);
 
-    Optional<List<Result>> findResultByUserEmail(String email);
+  Optional<List<Result>> findResultByUserEmail(String email);
 
-    @Query(value="SELECT r.accuracy FROM result r WHERE r.node_num = ?1", nativeQuery= true)
-    List<Double> findAccuracyByNodeNum(Integer nodeNum);
+  @Query(value = "SELECT r.accuracy FROM result r WHERE r.node_num = ?1 AND r.time IS NOT NULL", nativeQuery = true)
+  List<Double> findAccuracyByNodeNum(Integer nodeNum);
 
-    @Query(value="SELECT r.time FROM result r WHERE r.node_num = ?1", nativeQuery= true)
-    List<Double> findTimeByNodeNum(Integer nodeNum);
+  @Query(value = "SELECT r.time FROM result r WHERE r.node_num = ?1 AND r.time IS NOT NULL", nativeQuery = true)
+  List<Double> findTimeByNodeNum(Integer nodeNum);
 
 }
